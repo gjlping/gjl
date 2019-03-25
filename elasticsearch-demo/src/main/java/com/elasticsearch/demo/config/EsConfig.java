454b57d3ea0cf8acd23f1f4e77e9d3a72ec05e77
高锦铃 <gaojinling@zcmall.com>
2019-03-25 16:10:29 +0800

    es示例正序查询范围内部的人

---
+++ elasticsearch-demo/src/main/java/com/elasticsearch/demo/config/EsConfig.java
@@ -0,0 +1,23 @@
+package com.elasticsearch.demo.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by gjl on 2018/11/17.
+ */
+@Component
+@PropertySource("classpath:es-config.properties")
+@Data
+public class EsConfig {
+    @Value("${es.cluster.name}")
+    private String clusterName;
+    @Value("${es.host.ip}")
+    private String ip;
+    @Value("${es.host.port}")
+    private int port;
+}
